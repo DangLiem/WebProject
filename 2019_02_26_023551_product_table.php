9d0a0efc3473830de9be4e53071bb0f2e875ab2f
KhacNgoc <35987071+KhacNgoc@users.noreply.github.com>
2019-02-27 00:10:58 +0700

    migration.

---
+++ 2019_02_26_023551_product_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('idType')->nullable();
+            $table->string('description');
+            $table->string('image')->nullable();
+            $table->integer('quantity');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('product');
+    }
+}
